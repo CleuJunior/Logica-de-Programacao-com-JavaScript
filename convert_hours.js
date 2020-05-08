bdaa1fa4a6d4f9d875ece5ee069a3f45881ad764
CleuJunior <cleu.junior@gmail.com>
2020-05-08 20:03:43 -0300

    Convert Hours into Seconds

---
+++ convert_hours.js
@@ -0,0 +1,19 @@
+// Taks
+// Convert Hours into Seconds
+// Write a function that converts hours into seconds.
+
+// Examples
+// howManySeconds(2) ➞ 7200
+
+// howManySeconds(10) ➞ 36000
+
+// howManySeconds(24) ➞ 86400
+
+
+function howManySeconds(hours) {
+	return hours * 3600;
+}
+
+console.log(howManySeconds(2));
+console.log(howManySeconds(10));
+console.log(howManySeconds(24));
